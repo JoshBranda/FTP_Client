2118b44d3fb0d2eb74d4e30aecb9ec64d4ead415
Shayne Wang <shaynexwang@gmail.com>
2018-07-05 22:16:57 -0700

    added example for connection

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,10 +1,37 @@
-package main.java.client;
+package client;
 
+import java.io.IOException;
+import java.net.SocketException;
+
+import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPClientConfig;
 
 public class Client {
 	public static void main(String[] args) {
 		FTPClient ftp = new FTPClient();
+		FTPClientConfig config = new FTPClientConfig();
+		// user and pass for testing connections
+		String username = "anonymous";
+		String password = "anonymous";
 		System.out.println("running ftp client...");
+		// Test ftp client connection
+		String server = "speedtest.tele2.net";
+		try {
+			ftp.connect(server);
+		    System.out.println("Connected to " + server + ".");
+		    System.out.print(ftp.getReplyString());
+		    ftp.login(username, password);
+		    FTPFile [] files = ftp.listFiles();
+	        for (FTPFile file : files) {
+	            System.out.println(file.getName());
+	        }
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
