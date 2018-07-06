a1ed3985129d05013412c3c149b06860bbf19a41
Shayne Wang <shaynexwang@gmail.com>
2018-07-05 21:11:14 -0700

    initial setup

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,10 @@
+package main.java.client;
+
+import org.apache.commons.net.ftp.FTPClient;
+
+public class Client {
+	public static void main(String[] args) {
+		FTPClient ftp = new FTPClient();
+		System.out.println("running ftp client...");
+	}
+}
