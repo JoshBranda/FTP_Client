81a96e89875ce21435e679d297702b5a043676ea
Lee Hoang <leehoang97@gmail.com>
2018-08-01 20:00:47 -0700

    Removed unused import statements in Client.java.

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,15 +1,5 @@
 package client;
 
-import java.io.IOException;
-import java.net.SocketException;
-
-import org.apache.commons.net.ftp.FTPFile;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPClientConfig;
-
-// Lee's code for login prompts
-import java.util.Scanner; // For User input.
-
 public class Client {
 	public static void main(String[] args) {
 
