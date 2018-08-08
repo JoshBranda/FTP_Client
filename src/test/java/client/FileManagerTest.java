ddf5061f51c64da3dad06f1cffc5f31132f2fef2
Lee Hoang <leehoang97@gmail.com>
2018-08-07 22:02:49 -0700

    Added a @BeforeEach function to clean up the renamed file before each test.

--- src/test/java/client/FileManagerTest.java
+++ src/test/java/client/FileManagerTest.java
@@ -1,16 +1,21 @@
 //Used the following pages as resources:
 //https://stackoverflow.com/questions/1119385/junit-test-for-system-out-println
 //https://stackoverflow.com/questions/5694385/getting-the-filenames-of-all-files-in-a-folder
+//https://stackoverflow.com/questions/1158777/rename-a-file-using-java
 
 package client;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
+// For BeforeEach
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -31,6 +36,15 @@ public class FileManagerTest {
     public static void setUpStreams() {
         System.setOut(new PrintStream(outContent));
         System.setErr(new PrintStream(errContent));
+    }
+
+    @BeforeEach
+    public void setup() {
+        // Make sure to restore sample.txt after renameFileLS()
+        File sample = new File("src/main/resources/" + "sample.txt");
+        File pizza = new File("src/main/resources/" + "pizza.txt");
+        pizza.renameTo(sample);
+
     }
 
     @AfterAll
@@ -56,6 +70,7 @@ public class FileManagerTest {
 
     @Test
     public void renameFileLS() {
+        // This unit test looks for sample.txt in src/main/resources/ and renames it to pizza.txt.
         String original = "sample.txt";
         String test = "pizza.txt";
 
