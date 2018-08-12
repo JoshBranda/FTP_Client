e01b15af5fd8c3d61746e772af10fcac0f2b3772
Sappytomb796 <malice@pdx.edu>
2018-08-11 18:25:22 -0700

    Fix merge compile issues
    When pulling and pushing it did not change the Client class to
    the current version.  Remove depricated code that lingered.

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,10 +1,10 @@
 package client;
 
 public class Client {
-	//static private Menu menu;
+	static private Menu menu;
 
 	public static void main(String[] args) {
-
+		menu = new Menu();
 		LoginPrompts login = new LoginPrompts();
 
 		// user and pass for testing connections
@@ -12,33 +12,7 @@ public class Client {
 
 		login.getCreds();
 
-		FTPClient ftp = new FTPClient();
-		FTPClientConfig config = new FTPClientConfig();
-		Menu menu = new Menu();
-        // user and pass for testing connections
-		String username = "anonymous";
-		String password = "anonymous";
-		System.out.println("running ftp client...");
-		// Test ftp client connection
-		String server = "speedtest.tele2.net";
-		try {
-			ftp.connect(server);
-		    System.out.println("Connected to " + server + ".");
-		    System.out.print(ftp.getReplyString());
-		    ftp.login(username, password);
-		    FTPFile [] files = ftp.listFiles();
-	        for (FTPFile file : files) {
-	            System.out.println(file.getName());
-	        }
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-       menu.display();
+		menu.display();
 	}
 
 }
