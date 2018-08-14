5913cac0542915c45c1ff8f3e89b796060b2d4bf
Sappytomb796 <malice@pdx.edu>
2018-08-13 23:47:51 -0700

    Added option for changing permissions and updated local rename
    Option 8 will currently just call to change permissions on a
    file.
    
    Option 7 explicitly calls out that option 7 will now rename a local
    as well as calling the correct thing.

--- src/main/java/client/Menu.java
+++ src/main/java/client/Menu.java
@@ -13,6 +13,7 @@ public class Menu{
     protected static Scanner input = null;
 	protected static FileManager fm;
 	protected static RemoteFileManager rfm;
+	protected static RenameFileLocalServer rfls;
 
     public Menu(){
         input = new Scanner(System.in);
@@ -27,7 +28,7 @@ public class Menu{
     public static int display() {
 		String from;
 		String to;
-        //int num = 1;
+		int option;
 
         System.out.println("FTP Client: Please enter an option number or a command:");
         System.out.println("1. ls client");
@@ -36,17 +37,20 @@ public class Menu{
     	System.out.println("4. ls server");
         System.out.println("5. rm server file");
         System.out.println("6. mkdir server");
-        System.out.println("7. Rename File");
-        System.out.println("8. ");
+        System.out.println("7. Rename File On Local side");
+        System.out.println("8. Change Permissions on remote file");
         System.out.println("9. Quit");
 
-        switch (input.nextInt()) {
+		option = input.nextInt();
+		input.nextLine();
+        switch (option) {
             case 1:
 				fm.displayLocal();
 	            break;
     	    case 2:
 				System.out.print("Please enter the file to remove: ");
-				from = input.nextLine();
+				from = input.next();
+				input.nextLine();
 				rfm.removeFile(from);
 	            break;
     	    case 3:
@@ -70,15 +74,18 @@ public class Menu{
         	case 7:
 				System.out.println("Please enter the file name to change from: ");
 				from = input.nextLine();
-				System.out.println("Please enter the new file name:");
-				to = input.nextLine();
-				rfm.renameFile(from, to);
+				rfls.renameFile(from);
 	            break;
     	    case  8:
+				System.out.println("Please enter the file you want to change permissions on;");
+				to = input.nextLine();
+				System.out.println("Please enter the permissions;");
+				from = input.nextLine();
+				rfm.setPermissionRemote(from, to);
 	            break;
     	    case 9:
-	            System.out.println("Exiting");
-	            return 0;
+	            System.out.println("Loging out and exiting");
+				return 0;
         	default:
 	            System.out.println("Not a valid option");
 	            break;
